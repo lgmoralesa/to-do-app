e7a7f7649232944ae61f17ae550437d314daccbd
Laura Morales <lgmoralesa@unal.edu.co>
2017-08-17 20:35:47 +0000

    Controller autogenerate

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,9 @@ class WelcomeController < ApplicationController
 
     def index
         puts "Hola Mundo en consola"
+        
+        @imprimir=true
+        @nombre='laura'    #sin @ es local, con un @ la puede ver la vista, con @@ es global, la ven todos
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get '/about', to: 'index#about'
+
+  get '/contact', to: 'index#contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
    # get '/welcome', to:'welcome#index' #ese segundo welcome es un controlador 
-   root 'welcome#index' #redirige la ruta raiz a esa vista welcoem
+   root 'welcome#index' #redirige la ruta raiz a esa vista welcome
 
 
 
